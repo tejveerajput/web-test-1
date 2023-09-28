c32bcaad9fd137fc6647be6262668dfb5e02ea21
tejveerajput <tejveersingh275@gmail.com>
2023-09-28 14:10:48 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app=express();
+
+
+
+
+
+app.get('/',(req,res)=>{
+    res.send("Hello");
+})
+
+
+app.listen(8080,()=>{
+    console.log("server connected at 8080 port");
+})
